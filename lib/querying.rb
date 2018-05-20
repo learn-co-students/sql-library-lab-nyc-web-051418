c0c3f285bddf6fd83b37f2f6ed979c5b6d71109a
Alberto Carreras <a.carreras.c@gmail.com>
2018-05-20 19:36:42 -0400

    Solved lab / Bug on insert?

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,49 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year
+  FROM books
+  WHERE series_id = 1
+  ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto
+  FROM characters
+  ORDER BY length(motto) DESC
+  LiMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(*)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(*) DESC
+  LiMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
-end
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series
+  ON authors.id = author_id
+  JOIN subgenres
+  ON subgenres.id = subgenre_id"
+  end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title
+  FROM series
+  JOIN characters
+  ON series.id = series_id
+  GROUP BY title, species
+  ORDER BY COUNT(*) DESC
+  LIMIT 1"
 end
-
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT name, COUNT(*)
+  FROM characters
+  JOIN character_books
+  ON characters.id = character_id
+  GROUP BY characters.id
+  ORDER BY COUNT(*) DESC, name"
 end
